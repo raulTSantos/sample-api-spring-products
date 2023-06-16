bbef43832148505a34af28c8a1a6ccf0d23b9f61
raulTSantos <confox24.s@gmail.com>
2023-06-15 23:36:44 -0500

    removiedo validacion dto

--- src/main/java/com/company/dto/CategoryDTO.java
+++ src/main/java/com/company/dto/CategoryDTO.java
@@ -3,7 +3,6 @@ package com.company.dto;
 import java.util.Date;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,7 +14,6 @@ public class CategoryDTO {
 	private String id;
 
 	@NotBlank(message = "Ingrese un nombre valido.")
-	@NotEmpty(message = "este es un vacio")
 	private String name;
 
 	private Boolean enabled;
